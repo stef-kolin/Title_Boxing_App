35edadadc7108ec84e60ad89844da0c283b172fb
stef-kolin <142947789+stef-kolin@users.noreply.github.com>
2024-04-18 21:44:34 -0500

    Add overlay code
    added overlay code to index.html + bookingmenu.html

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+/*$("#confirmedbooking").on("click",function(){
+    $(document).getElementById("overlay").addClass("active");
+/*
+if($("")){
+
+}
+
+
+});
+*/
+
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
